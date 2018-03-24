c565da99d8859e06eb22c697ce8bc4c861515463
Andrew Zhu <dir01@dir01.org>
2018-03-24 21:48:26 +0300

    Form validation

--- AddForm.js
+++ AddForm.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
 import { 
@@ -14,7 +13,6 @@ import {
 } from '@shoutem/ui';
 
 
-
 export default class extends React.Component {
   componentWillMount() {
   	this.setState({
@@ -23,6 +21,7 @@ export default class extends React.Component {
 		sail: '',
 		time: this.getCurrentTimeString(),
 		'planned duration': '30',
+        errors: [],
 	})
   }
 
@@ -46,10 +45,7 @@ export default class extends React.Component {
 
         <Divider styleName={'section-header'} />
 
-		<Button 
-			style={{backgroundColor: 'lightgreen'}}
-			onPress={()=>{console.log(this.state)}}
-		>
+		<Button style={{backgroundColor: 'lightgreen'}} onPress={this.onSubmit.bind(this)}>
 			<Text>LOG SURF SESSION</Text>
 		</Button>
 
@@ -57,7 +53,28 @@ export default class extends React.Component {
     );
   }
 
+  onSubmit() {
+    const {surfer, sail, board, time} = this.state;
+    const surfSession = {
+        surfer, sail, board, time,
+        plannedDuration: this.state['planned duration']
+    };
+
+    const errors = {}
+    for (const key in surfSession) {
+        if (!surfSession[key]) {
+            errors[key] = 'Required';
+        }
+    }
+    if (Object.keys(errors).length) {
+        this.setState({errors})
+        return
+    }
+    this.props.onSubmit(surfSession);
+  }
+
   renderFormSection(name, renderSuggestions=false, inputValue='') {
+    const error = this.state.errors[name];
  	return (
 		<View>
         	<Divider styleName={'section-header'}>
@@ -66,8 +83,10 @@ export default class extends React.Component {
         	<TextInput
 				value={this.state[name] || inputValue}
 				onChangeText={(text) => {this.setState({[name]: text})}}
+                style={{paddingBottom: 10, placeholderTextColor: error ? 'red' : null}}
+                placeholder={error ? error : null}
 			/>
-			{ renderSuggestions && this.renderSuggestions(name) }
+            { renderSuggestions && this.renderSuggestions(name) }
   		</View>
 	)
   }
@@ -81,9 +100,10 @@ export default class extends React.Component {
 				    key={o}
                     style={{
 						backgroundColor: '#4A90E2',
-						borderColor: "black",
-						padding: 0, 
-						marginRight: 2, 
+                        borderColor: "black",
+                        borderRadius: 3,
+						padding: 0,
+						marginRight: 3, 
 					}}
 					onPress={()=>{this.setState({[name]: o})}}
 				><Text style={{paddingRight: 2, paddingLeft: 2, margin: 0}}>{o}</Text></Button>
