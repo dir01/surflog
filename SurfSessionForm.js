fc0e161c3c09c558614270824a2765e541e283c2
Andrew Zhu <dir01@dir01.org>
2018-04-13 19:10:36 +0300

    Redisigned SurfSessionForm

--- SurfSessionForm.js
+++ SurfSessionForm.js
@@ -1,17 +1,17 @@
 import uuidv4 from 'uuid/v4';
 import React from 'react';
 import {connect} from "react-redux";
-import {ScrollView} from 'react-native';
+import {ScrollView, StyleSheet} from 'react-native';
 import {
     View,
     Text,
-    TextInput,
-    Divider,
-    Caption,
     Tile,
     Title,
     Button,
+    Row,
+    TouchableOpacity,
 } from '@shoutem/ui';
+import TextInput from './TextInput';
 import moment from 'moment';
 
 
@@ -38,30 +38,31 @@ class AddForm extends React.Component {
 
     render() {
         return (
-            <ScrollView>
+            <ScrollView contentContainerStyle={styles.formContainer}>
+                <View>
 
-                <Tile styleName={'text-centric inflexible'}>
-                    <Title>{this.props.submitText}</Title>
-                </Tile>
+                    <Tile styleName={'text-centric inflexible'}>
+                        <Title>{this.props.submitText}</Title>
+                    </Tile>
 
-                {this.renderFormSection('surfer', renderSuggestions = true)}
+                    {this.renderFormSection('surfer', renderSuggestions = true)}
 
-                {this.renderFormSection('sail', renderSuggestions = true)}
+                    {this.renderFormSection('sail', renderSuggestions = true)}
 
-                {this.renderFormSection('board', renderSuggestions = true)}
+                    {this.renderFormSection('board', renderSuggestions = true)}
 
-                {this.renderFormSection('time', renderSuggestions = false)}
+                    {this.renderFormSection('time', renderSuggestions = false)}
 
-                {this.state.isEditing && this.renderFormSection('end time', renderSuggestions = false)}
+                    {this.state.isEditing && this.renderFormSection('end time', renderSuggestions = false)}
 
-                {!this.state.isEditing && this.renderFormSection('planned duration', renderSuggestions = false, inputValue = "30")}
+                    {!this.state.isEditing && this.renderFormSection('planned duration', renderSuggestions = false, inputValue = "30")}
 
-                <Divider styleName={'section-header'}/>
-
-                <Button style={{backgroundColor: 'lightgreen'}} onPress={this.onSubmit.bind(this)}>
-                    <Text>{this.props.submitText}</Text>
-                </Button>
-
+                </View>
+                <View>
+                    <Button style={styles.submitButton} onPress={this.onSubmit.bind(this)}>
+                        <Text style={styles.submitButtonText}>{this.props.submitText}</Text>
+                    </Button>
+                </View>
             </ScrollView>
         );
     }
@@ -87,23 +88,31 @@ class AddForm extends React.Component {
             return
         }
         this.props.onSubmit(surfSession);
+        this.componentWillMount()
     }
 
     renderFormSection(name, renderSuggestions = false, inputValue = '') {
         const error = this.state.errors[name];
+        let input;
         return (
             <View>
-                <Divider styleName={'section-header'}>
-                    <Caption>{name.toUpperCase()}</Caption>
-                </Divider>
-                <TextInput
-                    value={this.state[name] || inputValue}
-                    onChangeText={(text) => {
-                        this.setState({[name]: text})
-                    }}
-                    style={{paddingBottom: 10, placeholderTextColor: error ? 'red' : null}}
-                    placeholder={error ? error : null}
-                />
+                <Row>
+                    <TouchableOpacity style={styles.labelContainer} onPress={() => input.focus()}>
+                        <Text style={styles.labelText}>{name.toUpperCase()}</Text>
+                    </TouchableOpacity>
+                    <TextInput
+                        inputRef={element => {
+                            input = element
+                        }}
+                        value={this.state[name] || inputValue}
+                        selectTextOnFocus={true}
+                        onChangeText={(text) => {
+                            this.setState({[name]: text})
+                        }}
+                        style={{...styles.input, placeholderTextColor: error ? 'red' : null}}
+                        placeholder={error ? error : null}
+                    />
+                </Row>
                 {renderSuggestions && this.renderSuggestions(name)}
             </View>
         )
@@ -112,21 +121,15 @@ class AddForm extends React.Component {
     renderSuggestions(name) {
         const options = this.props.suggestItems[name] || [];
         return (
-            <View style={{flexDirection: 'row', marginBottom: 10}}>
+            <View style={styles.suggestionContainer}>
                 {options.map((o) => (
                     <Button
                         key={o}
-                        style={{
-                            backgroundColor: '#4A90E2',
-                            borderColor: "black",
-                            borderRadius: 3,
-                            padding: 0,
-                            marginRight: 3,
-                        }}
+                        style={styles.suggestionButton}
                         onPress={() => {
                             this.setState({[name]: o})
                         }}
-                    ><Text style={{paddingRight: 2, paddingLeft: 2, margin: 0}}>{o}</Text></Button>
+                    ><Text style={styles.suggestionText}>{o}</Text></Button>
                 ))}
             </View>
         )
@@ -136,5 +139,44 @@ class AddForm extends React.Component {
         return moment().format('HH:mm');
     }
 }
+
+
+const styles = {
+    formContainer: {flex: 1, justifyContent: 'space-between'},
+    labelContainer: {
+        alignSelf: 'stretch',
+        flexDirection: 'row',
+        flex: 1
+    },
+    labelText: {
+        flexDirection: 'row',
+        alignSelf: 'center',
+    },
+    input: {
+        paddingBottom: 10,
+        borderWidth: 1,
+        borderColor: 'lightgray',
+        flex: 4,
+    },
+    suggestionContainer: {flexDirection: 'row', marginBottom: 10, marginLeft: 15, backgroundColor: 'white'},
+    suggestionButton: {
+        backgroundColor: '#999',
+        borderColor: '#999',
+        borderRadius: 4,
+        padding: 5,
+        marginRight: 5,
+    },
+    suggestionText: {
+        paddingRight: 2,
+        paddingLeft: 2,
+        margin: 0,
+        color: 'white',
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
+    submitButton: {backgroundColor: 'lightgreen', padding: 20},
+    submitButtonText: {fontSize: 20},
+};
+
 
 export default addSuggestItemsToState(AddForm);
